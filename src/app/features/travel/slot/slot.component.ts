905848cbb7f88441e6840a10150f4470a199a186
Nicolas <nicolas.dauzet@gmail.com>
2026-05-10 03:59:05 +0200

    update name

--- src/app/features/travel/slot/slot.component.ts
+++ src/app/features/travel/slot/slot.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { Activity, Slot } from '../../../core/models/travel.models';
 import { ActivityComponent } from '../activity/activity.component';
 import { SafeHtmlPipe } from '../../../shared/pipes/safe-html.pipe';
@@ -15,6 +15,13 @@ export class SlotComponent {
   private readonly travel = inject(TabService);
   readonly slot = input.required<Slot>();
 
+  readonly editingField = signal<'time' | 'name' | null>(null);
+
+  save(field: 'time' | 'name', value: string): void {
+    this.travel.updateSlotField(this.slot().id, { [field]: value });
+    this.editingField.set(null);
+  }
+
   addActivity(): void {
   const newActivity: Activity = {
     id: Date.now(), // ou un uuid, selon ton modèle
@@ -26,5 +33,5 @@ export class SlotComponent {
 
   const updatedActivities = [...(this.slot().activities ?? []), newActivity];
   this.travel.updateSlotField(this.slot().id, { activities: updatedActivities });
-}
+  }
 }
