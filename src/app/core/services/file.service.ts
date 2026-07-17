a561e6252325e8014f049892f3cee454f8aed265
Nicolas <nicolas.dauzet@gmail.com>
2026-07-17 23:56:26 +0200

    correctif couleur nav + file service

--- src/app/core/services/file.service.ts
+++ src/app/core/services/file.service.ts
@@ -1,21 +1,32 @@
 import { inject, Injectable } from '@angular/core';
 import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
-import { from, Observable, switchMap } from 'rxjs';
+import { from, Observable, map, switchMap } from 'rxjs';
 import { FirebaseService } from '@core/infra/firebase/firebase.service';
+
+export interface UploadedFile {
+  url: string;
+  name: string;
+  path: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class FileService {
   private readonly storage = getStorage(inject(FirebaseService).app);
 
-  uploadFile(file: File, path: string): Observable<{ url: string; name: string }> {
+  uploadFile(file: File, path: string): Observable<UploadedFile> {
     const storageRef = ref(this.storage, path);
     return from(uploadBytes(storageRef, file)).pipe(
       switchMap(() => from(getDownloadURL(storageRef))),
-      switchMap((url: any) => [{ url, name: file.name }]),
+      map((url: string) => ({
+        url,
+        name: file.name,
+        path: path
+      }))
     );
   }
 
   deleteFile(path: string): Observable<void> {
-    return from(deleteObject(ref(this.storage, path)));
+    const storageRef = ref(this.storage, path);
+    return from(deleteObject(storageRef));
   }
 }
